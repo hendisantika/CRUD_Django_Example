88e98edfd0db7ba08314b6ba9b0848719dc5dfa5
hendisantika <hendisantika@yahoo.co.id>
2019-02-07 06:12:22 +0700

    #13 - Modify urls.py

--- CRUD_Django_Example/urls.py
+++ CRUD_Django_Example/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+
+from CRUD_FBVs import views
+
+app_name = 'CRUD_FBVs'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url(r'^$', views.movies_list, name='movies_list'),
+    url(r'^new$', views.movies_create, name='movies_new'),
+    url(r'^edit/(?P<pk>\d+)$', views.movies_update, name='movies_edit'),
+    url(r'^delete/(?P<pk>\d+)$', views.movies_delete, name='movies_delete'),
 ]
