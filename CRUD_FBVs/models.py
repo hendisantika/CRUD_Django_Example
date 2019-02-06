41920b48a3ccac760a9ce37b8074d5ac1408405a
hendisantika <hendisantika@yahoo.co.id>
2019-02-07 06:00:01 +0700

    #4 - Create Movie model

--- CRUD_FBVs/models.py
+++ CRUD_FBVs/models.py
@@ -1,1 +1,22 @@
 # Create your models here.
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class Movies(models.Model):
+
+
+'''''by specifying user as Foreign Key, we're using the builtin User model of django.'''
+
+user = models.ForeignKey(User, on_delete=models.CASCADE)
+title = models.CharField(max_length=200)
+genre = models.CharField(max_length=200)
+
+
+def __unicode__(self):
+    return self.title
+
+
+def get_absolute_url(self):
+    return reverse('CRUD_FBVs:movies_edit', kwargs={'pk': self.pk})
