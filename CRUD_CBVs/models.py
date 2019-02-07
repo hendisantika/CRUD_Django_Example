326821309c425a72a00af34733e479773491c703
hendisantika <hendisantika@yahoo.co.id>
2019-02-08 05:45:27 +0700

    #19 - Add Movie Model - Class Based Views (CBV)

--- CRUD_CBVs/models.py
+++ CRUD_CBVs/models.py
@@ -1,1 +1,14 @@
 # Create your models here.
+from django.db import models
+from django.urls import reverse
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.IntegerField()
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('CRUD_CBVs:movie_edit', kwargs={'pk': self.pk})
